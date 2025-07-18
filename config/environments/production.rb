7cb75f3b447ae772a31d38115f33a7b33e973226
dima <dima@cloud66.com>
2025-07-18 18:30:22 +0100

    Renamed group

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   if ENV.fetch("REDIS_URL", nil).present?
     config.cache_store = :redis_cache_store, {
+      name: "primary",
       url: ENV.fetch("REDIS_URL"),
       sentinels: [{ host: ENV.fetch("REDIS_ADDRESS"), port: 26_379 }],
       role: 'master',
