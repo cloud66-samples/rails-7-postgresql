ea36fe671eab7eae818262922b94c3a18e6e16e0
dima <dima@cloud66.com>
2025-09-24 19:32:30 +0100

    Revert "Update welcome_controller.rb"
    This reverts commit 6987cf953e514fec1d1f1c5d0c5b8c0adf31da75.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,2 @@
 class WelcomeController < ApplicationController
-  def index
-    @time_now = ::Time.now
-    @redis_time_now = ::Rails.cache.fetch("redis_time_now", expires_in: 5.seconds) { ::Time.now }
-  end
 end
